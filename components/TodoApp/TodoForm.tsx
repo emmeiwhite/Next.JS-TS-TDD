e03b51136fe055dff504946a51380ab4a7b50e29
emmeiwhite <emmeiwhite@gmail.com>
2025-07-07 23:57:06 +0530

    07. User can now type a Todo Item

--- components/TodoApp/TodoForm.tsx
+++ components/TodoApp/TodoForm.tsx
@@ -10,20 +10,23 @@ export default function TodoForm() {
   }
 
   return (
-    <form
-      className="flex"
-      onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="todoItem"
-        placeholder="Enter your Todo"
-        value={todoItem}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTodoItem(e.target.value)}
-        className="p-2 placeholder:text-gray-400 flex-1 hover:ring-2 border-1 border-blue-500 rounded-sm"
-      />
-      <button className=" bg-blue-500 rounded-sm px-5 py-2 text-white cursor-pointer hover:bg-blue-600 transition-colors duration-300 ">
-        Add Todo
-      </button>
-    </form>
+    <div className="p-3">
+      <h1 className="text-xl font-bold text-center mb-3">Enter Your Todos</h1>
+      <form
+        className="flex"
+        onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="todoItem"
+          placeholder="Enter your Todo"
+          value={todoItem}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTodoItem(e.target.value)}
+          className="p-2 placeholder:text-gray-400 flex-1 hover:ring-2 border-1 border-blue-500 rounded-sm"
+        />
+        <button className="bg-blue-500 rounded-sm px-5 py-2 text-white cursor-pointer hover:bg-blue-600 transition-colors duration-300 ">
+          Add Todo
+        </button>
+      </form>
+    </div>
   )
 }
