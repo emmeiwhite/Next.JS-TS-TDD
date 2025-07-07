c9e0fb5847282a528eb13f2a2186e7537a04c9de
emmeiwhite <emmeiwhite@gmail.com>
2025-07-07 23:41:57 +0530

    06-Types provided, FormEvent & ChangeEvent

--- components/TodoApp/TaskManager.tsx
+++ components/TodoApp/TaskManager.tsx
@@ -1,3 +1,18 @@
+import TodoForm from './TodoForm'
+import TodoItems from './TodoItems'
+
 export default function TaskManager() {
-  return <div>TaskManager</div>
+  return (
+    <div className="bg-gray-100 min-h-[calc(100vh-120px)]">
+      <div className="max-w-5xl mx-auto px-5 sm:px-6 flex items-center justify-between py-4">
+        {/* Todo Container */}
+        <div className="w-3/5 mx-auto mt-12 p-4 shadow-gray-400 shadow-lg">
+          {/* TodoForm */}
+          <TodoForm />
+          {/* TodoItems */}
+          <TodoItems />
+        </div>
+      </div>
+    </div>
+  )
 }
--- components/TodoApp/TodoForm.tsx
+++ components/TodoApp/TodoForm.tsx
@@ -1,3 +1,29 @@
+'use client'
+
+import { useState } from 'react'
+
 export default function TodoForm() {
-  return <div>TodoForm</div>
+  const [todoItem, setTodoItem] = useState<string>('')
+
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault()
+  }
+
+  return (
+    <form
+      className="flex"
+      onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="todoItem"
+        placeholder="Enter your Todo"
+        value={todoItem}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTodoItem(e.target.value)}
+        className="p-2 placeholder:text-gray-400 flex-1 hover:ring-2 border-1 border-blue-500 rounded-sm"
+      />
+      <button className=" bg-blue-500 rounded-sm px-5 py-2 text-white cursor-pointer hover:bg-blue-600 transition-colors duration-300 ">
+        Add Todo
+      </button>
+    </form>
+  )
 }
