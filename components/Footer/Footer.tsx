f65721ec6ad0aa109b97a18ea6d8bccf9afb8890
emmeiwhite <emmeiwhite@gmail.com>
2025-07-07 22:58:20 +0530

    05-Header/Footer simple

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,3 +1,10 @@
 export default function Footer() {
-  return <div>Footer</div>
+  return (
+    <div className="bg-gradient-to-tr from-teal-300 to-teal-400">
+      <div className="max-w-5xl mx-auto px-5 sm:px-6 flex items-center justify-between py-4">
+        <h2 className="font-extrabold text-blue-900 ">TaskManager</h2>
+        <p>Copyright &copy; Imran Rafiq Rather {new Date().getFullYear()}</p>
+      </div>
+    </div>
+  )
 }
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Header() {
   return (
-    <div className="bg-gradient-to-tr from-purple-300 to-purple-400">
+    <div className="bg-gradient-to-tr from-teal-300 to-teal-400">
       <div className="max-w-5xl mx-auto px-5 sm:px-6 flex items-center justify-between py-4">
         <h2 className="font-extrabold text-blue-900 text-2xl">TaskManager</h2>
         <div>
