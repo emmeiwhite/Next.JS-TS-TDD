e8fcea0ce5c66b99c2c12e3795a90449cc180caa
emmeiwhite <emmeiwhite@gmail.com>
2025-07-09 20:27:01 +0530

    32. Run-Time API type-checking with zod

--- app/tours/page-without-zod-fetch.tsx
+++ app/tours/page-without-zod-fetch.tsx
@@ -1,37 +1,25 @@
-// ✅ Step 1: Install zod
-import { z } from 'zod'
-
-// ✅ Step 2: Define a Zod Schema for Tour
-const TourSchema = z.object({
-  id: z.string(),
-  name: z.string(),
-  info: z.string(),
-  image: z.string(),
-  price: z.string()
-})
-
-// ✅ Step 3: Infer  TS type
-type Tour = z.infer<typeof TourSchema>
-
 const url: string = 'https://www.course-api.com/react-tours-project'
 
-// ✅ Step 4: Validate the API response inside fetchTours
+type Tour = {
+  id: string
+  name: string
+  info: string
+  image: string
+  price: string
+}
+
 async function fetchTours(url: string): Promise<Tour[]> {
   // Just a 1s delay -- simulating the API
   await new Promise(resolve => setTimeout(resolve, 1000))
 
   try {
     const response = await fetch(url)
-    const rawData: Tour[] = await response.json()
-
-    // SafeParsing
-    const result = TourSchema.array().safeParse(rawData)
-
-    if (!result.success) {
-      throw new Error(`Invalid data: ${result.error}`)
+    if (!response.ok) {
+      throw new Error('Could not fetch Tours')
     }
 
-    return result.data
+    const data: Tour[] = await response.json()
+    return data
   } catch (error) {
     const errorMsg = error instanceof Error ? error.message : 'there was an error ...'
 
@@ -44,7 +32,17 @@ async function fetchTours(url: string): Promise<Tour[]> {
 /** --- Server Side Rendering in Next.js --- */
 export default async function ToursPage() {
   const tours = await fetchTours(url)
-
+  console.log(tours)
+  const toursJSX: React.ReactNode = tours?.map((tour: Tour) => {
+    return (
+      <div
+        key={tour.id}
+        className="p-3 rounded shadow-sm shadow-indigo-400">
+        <h2>{tour.name}</h2>
+        <p>{tour.price}</p>
+      </div>
+    )
+  })
   return (
     <div className="py-6">
       <div className="max-w-5xl mx-auto px-5 sm:px-6">
@@ -54,18 +52,7 @@ export default async function ToursPage() {
 
         <h2 className="my-3 text-xl font-bold">Total Tours: {tours.length}</h2>
 
-        <div className="flex flex-col gap-4">
-          {tours.map(tour => {
-            return (
-              <div
-                key={tour.id}
-                className="p-3 rounded shadow-sm shadow-indigo-400">
-                <h2>{tour.name}</h2>
-                <p>{tour.price}</p>
-              </div>
-            )
-          })}
-        </div>
+        <div className="flex flex-col gap-4">{toursJSX}</div>
       </div>
     </div>
   )
