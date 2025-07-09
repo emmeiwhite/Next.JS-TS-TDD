3fc7afd9b244add1e68196d26f035c7c44e8e00d
emmeiwhite <emmeiwhite@gmail.com>
2025-07-09 18:18:01 +0530

    30. Challenge Completed ! Praise be to only One God

--- components/Challenges/Challenge1.tsx
+++ components/Challenges/Challenge1.tsx
@@ -1,8 +1,10 @@
+/* --- Not a good solution for condition prop rendering
 type PropType = {
   type: 'basic' | 'advanced'
   name: string
   email?: string
 }
+
 
 export default function Challenge1(props: PropType) {
   const { type, name, email } = props
@@ -12,6 +14,37 @@ export default function Challenge1(props: PropType) {
     <div className={`${classes} p-3 rounded border-2`}>
       <h2 className="text-xl">{name}</h2>
       <p className="text-base ">{type === 'advanced' ? email : ''}</p>
+    </div>
+  )
+}
+--- */
+
+type BasicPropType = {
+  type: 'basic'
+  name: string
+}
+
+type AdvancedPropType = {
+  type: 'advanced'
+  name: string
+  email: string
+}
+
+export default function Challenge1(props: BasicPropType | AdvancedPropType) {
+  const { type, name } = props
+
+  if (type === 'basic') {
+    return (
+      <div className={`bg-green-200 border-green-500 p-3 rounded border-2`}>
+        <h2 className="text-xl">{name}</h2>
+      </div>
+    )
+  }
+
+  return (
+    <div className={`border-red-500 bg-red-200 p-3 rounded border-2`}>
+      <h2 className="text-xl">{name}</h2>
+      <p className="text-base ">{props.email}</p>
     </div>
   )
 }
