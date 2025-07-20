591e99ca571ce67160a0633848897dedd2fa874e
emmeiwhite <emmeiwhite@gmail.com>
2025-07-20 23:51:10 +0530

    33. Revisit: We must must must not use interactivity within Server Components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import TaskManager from '@/components/TodoApp/TaskManager'
 
 export default function Home() {
+  function handleClick() {
+    console.log('I am a server component, do not use event handlers here mate!')
+  }
+
   return (
     <div className="">
       <TaskManager />
+      <button onClick={handleClick}>Click Me! Testing</button>
     </div>
   )
 }
