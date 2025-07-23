c02f1307dee0f8e9dbd129f048b3970fda5c148f
emmeiwhite <emmeiwhite@gmail.com>
2025-07-23 19:06:13 +0530

    43. void return type covered

--- typescript/functions.ts
+++ typescript/functions.ts
@@ -12,3 +12,19 @@ addStrings('hello', 'world')
 
 const subtractNumbers = (a: number, b: number): number => a - b
 subtractNumbers(34, 5)
+
+// default parameters
+const greeting = (name: string, greet: string = 'Welcome') => ` ${greet}: ${name}`
+greeting('roufi')
+
+// union type for parameter
+const playAround = (name: string, play: string | boolean) => ` ${name} is playing: ${play}`
+playAround('Mr. Rouf', true)
+playAround('Mr. Rouf', 'cricket')
+
+// void type
+const doTask = (name: string): void => {
+  console.log(`${name} do your task`)
+}
+
+doTask('imran')
