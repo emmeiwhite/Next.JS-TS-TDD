867e9b76c2afa1d12dd61965841778a4699dd159
emmeiwhite <emmeiwhite@gmail.com>
2025-07-25 06:46:19 +0530

    50. Function returning a function - Classic Example of Closures

--- typescript/higher-order-functions.ts
+++ typescript/higher-order-functions.ts
@@ -17,8 +17,18 @@ const mulBy10 = (v: number) => v * 10
 type MutationFunction = (v: number) => number
 
 function arrayMutate(arr: number[], mutate: MutationFunction): number[] {
-  return arr.map(mulBy10)
+  return arr.map(mutate)
 }
 
 const modifiedArray = arrayMutate([2, 4, 6], mulBy10)
 console.log(modifiedArray)
+
+/** Functions that return function. A classic JavaScript Closure */
+type AdderFunction = (val: number) => number
+
+function createAdder(num: number): AdderFunction {
+  return (val: number) => val + num
+}
+
+const add10 = createAdder(10)
+console.log(add10(5))
