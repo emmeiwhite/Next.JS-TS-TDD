b9a103e37401799459a9af706983bb3e7507d91f
emmeiwhite <emmeiwhite@gmail.com>
2025-07-27 00:07:26 +0530

    58. Revisit --- typescript children prop with type defined separately

--- components/Playground/PlaygroundBox.tsx
+++ components/Playground/PlaygroundBox.tsx
@@ -1,5 +1,7 @@
 'use client'
 
-export default function PlaygroundBox({ children }: { children: React.ReactNode }) {
+type BoxType = { children: React.ReactNode }
+
+export default function PlaygroundBox({ children }: BoxType) {
   return <div>{children}</div>
 }
