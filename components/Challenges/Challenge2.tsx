bbbe09f69208881607b61ecd6d258ab505e1dd91
emmeiwhite <emmeiwhite@gmail.com>
2025-07-30 21:48:42 +0530

    92. ContextAPI --- No More Prop-Drilling!

--- components/Challenges/Challenge2.tsx
+++ components/Challenges/Challenge2.tsx
@@ -1,6 +1,7 @@
 'use client'
 import Link from 'next/link'
-import { useState } from 'react'
+import { useContext, useState } from 'react'
+import { createContext } from 'react'
 
 type Link = {
   link: string
@@ -24,6 +25,18 @@ type UserType = {
   login: () => void
 }
 
+// 1. Create Context
+const NavbarContext = createContext<UserType | null>(null)
+
+// custom context --- save lines of codes
+export const useCustomContext = () => {
+  const context = useContext(NavbarContext)
+  if (!context) {
+    throw new Error('useCustomContext must be used within a NavbarContext.Provider')
+  }
+  return context
+}
+
 export default function NavbarChallenge() {
   const [user, setUser] = useState<string | null>('Roufi')
 
@@ -37,19 +50,22 @@ export default function NavbarChallenge() {
     setUser('Roufi')
   }
 
+  const valueState: UserType | null = {
+    user,
+    logout,
+    login
+  }
   return (
-    <div className="flex space-between items-center py-2 gap-5 bg-blue-200 px-4">
-      <h2 className="text-xl font-bold">Context API</h2>
-      <NavLinks
-        user={user}
-        logout={logout}
-        login={login}
-      />
-    </div>
+    <NavbarContext.Provider value={valueState}>
+      <div className="flex space-between items-center py-2 gap-5 bg-blue-200 px-4">
+        <h2 className="text-xl font-bold">Context API</h2>
+        <NavLinks />
+      </div>
+    </NavbarContext.Provider>
   )
 }
 
-function NavLinks({ user, logout, login }: UserType) {
+function NavLinks() {
   return (
     <div className="flex space-between items-center gap-4 flex-1">
       <ul className="flex space-between items-center gap-4 flex-1">
@@ -65,16 +81,13 @@ function NavLinks({ user, logout, login }: UserType) {
         })}
       </ul>
 
-      <UserContainer
-        user={user}
-        logout={logout}
-        login={login}
-      />
+      <UserContainer />
     </div>
   )
 }
 
-function UserContainer({ user, logout, login }: UserType) {
+function UserContainer() {
+  const { user, logout, login } = useCustomContext()
   return (
     <div className="flex space-between items-center gap-4 ">
       {user === null ? (
@@ -88,7 +101,7 @@ function UserContainer({ user, logout, login }: UserType) {
         </div>
       ) : (
         <div className="flex gap-3 items-center">
-          <p className="font-bold text-green-900">{user}</p>
+          <p className="font-bold text-green-900">Hello, {user}</p>
           <button
             className="bg-blue-600 rounded px-3 py-1 text-white cursor-pointer"
             onClick={logout}>
