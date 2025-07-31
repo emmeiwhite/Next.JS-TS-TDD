81b96835558287281891bda562b5c84846f5c51c
emmeiwhite <emmeiwhite@gmail.com>
2025-08-01 01:23:48 +0530

    98. Wonderfully crafted the art of reset

--- components/Playground/App.tsx
+++ components/Playground/App.tsx
@@ -9,17 +9,37 @@ type User = {
   name: string
 }
 
+type stateType = {
+  users: typeof people
+  isLoading: boolean
+  isError: string
+}
+type actionType = { type: string; items?: typeof people }
+
+// type reducerType = (state:stateType,action:actionType)=>
+
+// Default State
 const initialState = {
-  users: people,
+  users: [],
   isLoading: false,
   isError: ''
 }
 
+// Actions
+const clear_all = 'clear_all'
+const reset = 'reset'
+const set_items = 'set_items'
 // reducer may seem as if we are reducing something, Please do not be misled by naming. Reducer function is a pure function which actually returns an updated form of our state everytime user performs an action. And React team has also named that thing within React as action object with a type.
-function reducer(state, action) {
-  if (action.type === 'clear_all') {
+function reducer(state: stateType, action: actionType) {
+  if (action.type === clear_all) {
     return { ...state, users: [] }
   }
+
+  if (action.type === reset) {
+    return { ...state, users: action.items }
+  }
+
+  return state
 }
 
 export default function App() {
@@ -38,16 +58,20 @@ export default function App() {
       if (parsed.length !== 0) {
         console.log('From Local Storage')
         // setUsers(parsed)
+        dispatch({ type: set_items, items: parsed })
       } else {
         console.log('First time or after all items are deleted!')
+        console.log(parsed)
         // setUsers(people)
+        dispatch({ type: reset, items: people })
       }
     }
   }, [])
 
   // Load from localStorage only after the component has mounted (avoids hydration error)
   useEffect(() => {
-    getAllUsers()
+    // getAllUsers()
+    dispatch({ type: reset, items: people })
   }, [getAllUsers])
 
   // Sync any user changes to localStorage
